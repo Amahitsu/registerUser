00e42acfe8d7954b1075eb70d0002e02cf087396
AMANDA HIRATA MEDEIROS <amanda.medeiros1@alunos.sc.senac.br>
2025-03-11 20:58:39 -0300

    adicionando funcionalidades de cubrir senha e visualizar com icone

--- app/src/main/java/com/example/registeruser/screens/RegisterUserMainScreen.kt
+++ app/src/main/java/com/example/registeruser/screens/RegisterUserMainScreen.kt
@@ -14,9 +14,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.registeruser.components.MyTextField
 import com.example.registeruser.ui.theme.RegisterUserTheme
 
 @Composable
@@ -41,52 +43,82 @@ fun RegisterUserFields(registerUserViewModel: RegisterUserViewModel){
     var registerUser = registerUserViewModel.uiState.collectAsState()
     val ctx = LocalContext.current
 
-
-    OutlinedTextField(value = registerUser.value.user,
-        onValueChange = {
-                        registerUserViewModel.onUserChange(it)
-        },
-        singleLine = true,
-        label = {
-            Text(text = "User")
-        },
-        modifier = Modifier.fillMaxWidth()
+    MyTextField(label = "User",
+        value = registerUser.value.user,
+        onValueChange = {registerUserViewModel.onUserChange(it)}
     )
 
-    OutlinedTextField(value = registerUser.value.email,
-        onValueChange = {
-                        registerUserViewModel.onEmailChange(it)
-        },
-        singleLine = true,
-        label = {
-            Text(text = "Email")
-        },
-        modifier = Modifier.fillMaxWidth()
-
+    MyTextField(label = "Email",
+        value = registerUser.value.email,
+        onValueChange = {registerUserViewModel.onEmailChange(it)}
     )
 
-    OutlinedTextField(value = registerUser.value.password,
+    MyTextField(label = "Password",
+        value = registerUser.value.password,
         onValueChange = {registerUserViewModel.onPasswordChange(it)},
-        singleLine = true,
-        label = {
-            Text(text = "Password")
-        },
         visualTransformation = PasswordVisualTransformation(),
-        modifier = Modifier.fillMaxWidth()
+        isPassword = true
     )
 
-    OutlinedTextField(value = registerUser.value.confirmPassword,
+    MyTextField(label = "Password Confirm",
+        value = registerUser.value.confirmPassword,
         onValueChange = {registerUserViewModel.onConfirmPassword(it)},
-        singleLine = true,
-        label = {
-            Text(text = "Password confirm")
-        },
         visualTransformation = PasswordVisualTransformation(),
-        modifier = Modifier.fillMaxWidth()
+        isPassword = true
     )
+
+
+    //OutlinedTextField(value = registerUser.value.user,
+//        onValueChange = {
+//                        registerUserViewModel.onUserChange(it)
+//        },
+//        singleLine = true,
+//        label = {
+//            Text(text = "User")
+//        },
+    //    isError = registerUser.value.user.isBlank(),
+    //    modifier = Modifier.fillMaxWidth()
+    //)
+
+//    OutlinedTextField(value = registerUser.value.email,
+//        onValueChange = {
+//                        registerUserViewModel.onEmailChange(it)
+//        },
+//        singleLine = true,
+//        label = {
+//            Text(text = "Email")
+//        },
+//        isError = registerUser.value.email.isBlank(),
+//        modifier = Modifier.fillMaxWidth()
+//
+//    )
+//
+//    OutlinedTextField(value = registerUser.value.password,
+//        onValueChange = {registerUserViewModel.onPasswordChange(it)},
+//        singleLine = true,
+//        label = {
+//            Text(text = "Password")
+//        },
+//        visualTransformation = PasswordVisualTransformation(),
+//        isError = registerUser.value.password.isBlank(),
+//        modifier = Modifier.fillMaxWidth()
+//    )
+//
+//    OutlinedTextField(value = registerUser.value.confirmPassword,
+//        onValueChange = {registerUserViewModel.onConfirmPassword(it)},
+//        singleLine = true,
+//        label = {
+//            Text(text = "Password confirm")
+//        },
+//        visualTransformation = PasswordVisualTransformation(),
+//        isError = registerUser.value.confirmPassword.isBlank(),
+//        modifier = Modifier.fillMaxWidth()
+//    )
 
     Button(
         modifier = Modifier.padding(16.dp),
+
+
         onClick = {
 
             Toast.makeText(ctx, "Mensagem", Toast.LENGTH_SHORT).show()
